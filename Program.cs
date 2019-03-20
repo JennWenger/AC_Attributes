8552299a0a6f68ab4413c4b52c7065d9ca5172cb
Jennifer Wenger <jennifer@techiewebdesigns.com>
2019-03-20 10:26:00 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+using System.Xml;
+
+namespace LearningLine.Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = new SalesOrder
+            {
+                Number = 42,
+                Customer = "Big Bucks Supermart",
+                Notes = "This customer has lots of money. Do not apply discount.",
+            };
+
+            var xml = SalesOrderToXml(order);
+            Console.WriteLine(xml);
+            Console.ReadKey(true);
+        }
+
+        static string SalesOrderToXml(SalesOrder order)
+        {
+            XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+            ns.Add("", "");
+
+            var result = new StringBuilder();
+            var settings = new XmlWriterSettings
+            {
+                OmitXmlDeclaration = true,
+                Indent = true
+            };
+            var stream = XmlTextWriter.Create(result, settings);
+            var ser = new XmlSerializer(typeof(SalesOrder));
+            ser.Serialize(stream, order, ns);
+            return result.ToString();
+        }
+    }
+}
---
+++ SalesOrderLib/SalesOrder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.Reflection;
+
+namespace LearningLine.Practice
+{
+   
+    [XmlRoot("order")]
+    public class SalesOrder
+    {
+        public string Customer { get; set; }
+        [XmlElement("orderid", Order = 1)]
+        public int Number { get; set; }
+        [XmlIgnore]
+        public string Notes { get; set; }
+        [XmlAttribute("version")]
+        public string Version
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+            set
+            {
+                // ignore
+            }
+        }
+    }
+}
